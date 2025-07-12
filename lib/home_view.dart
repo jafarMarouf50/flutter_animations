a110f86ac15ac7d186e641a0fdd84244a6213ce5
Jafar Marouf <18597109-JafarKMarouf@users.noreply.gitlab.com>
2025-07-12 16:46:54 +0300

    use animated rotation to rotate container

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -8,7 +8,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  double _area = 100;
+  double _turns = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +23,24 @@ class _HomeViewState extends State<HomeView> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [_animatedContainer(),_resizeContainerButton()],
+          children: [_animatedContainer(), _resizeContainerButton()],
         ),
       ),
     );
   }
 
   Widget _animatedContainer() {
-    return AnimatedContainer(
+    return AnimatedRotation(
       duration: Duration(seconds: 2),
-      width: _area,
-      height: _area,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.blue,
+      curve: Curves.easeIn,
+      turns: _turns,
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.blue,
+        ),
       ),
     );
   }
@@ -45,10 +49,10 @@ class _HomeViewState extends State<HomeView> {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
       onPressed: () {
-        _area = _area == 200 ? 150 : 200;
+        _turns += 1.25;
         setState(() {});
       },
-      child: Text("Resize Container", style: TextStyle(color: Colors.white)),
+      child: Text("Rotate Container", style: TextStyle(color: Colors.white)),
     );
   }
 }
